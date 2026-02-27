1c72d376e688544e4abb35d1ea3b53c01fc4b678
Brendha Casaro <brendhameira9@gmail.com>
2026-02-26 22:24:50 -0300

    .

---
+++ src/main/java/com/gamesheets/gamesheets/GamesheetsApplication.java
@@ -0,0 +1,13 @@
+package com.gamesheets.gamesheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesheetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamesheets/gamesheets/GamesheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamesheets.gamesheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
